50085a2d7fd6623719b4347aeaed05279b55fa42
Ghaeth Mrad <gaethmrad49@gmail.com>
2024-08-18 19:51:06 +0000

    button scroll to top

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let toggle = document.getElementById("toggle");
 let drobdown = document.querySelector(".drobdown");
+let scrollBtn = document.getElementById("scroll-to-top");
 
 toggle.onclick = function() {
   drobdown.classList.toggle("active")
@@ -9,3 +10,16 @@ document.addEventListener("click",(e) => {
     drobdown.classList.remove("active")
   }
 })
+
+const scrollBtnDisplay = function () {
+  window.scrollY > window.innerHeight
+    ? scrollBtn.classList.add("show")
+    : scrollBtn.classList.remove("show");
+};
+window.addEventListener("scroll", scrollBtnDisplay);
+const scrollWindow = function () {  
+  window.scrollTo({
+    top: 0,
+  })
+};
+scrollBtn.addEventListener("click", scrollWindow);
